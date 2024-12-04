53d54d7109dbe724faea382535992b5c5eb2fbaa
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-12-04 20:46:27 -0300

    Adicionando camada de serviços

--- src/main/java/com/bemobi/domain/service/UrlShortenerService.java
+++ src/main/java/com/bemobi/domain/service/UrlShortenerService.java
@@ -1,2 +1,42 @@
-package com.bemobi.domain.service;public class UrlShortenerService {
+package com.bemobi.domain.service;
+
+import com.bemobi.domain.exception.CustomAliasAlreadyExistsException;
+import com.bemobi.domain.exception.UrlNotFoundException;
+import com.bemobi.domain.model.UrlShortener;
+import com.bemobi.domain.repository.UrlShortenerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+public class UrlShortenerService {
+
+    @Autowired
+    private UrlShortenerRepository repository;
+
+    @Transactional
+    public UrlShortener save(UrlShortener urlShortener){
+
+        if (urlShortener.getCustomAlias() != null) {
+            if (repository.existsByAlias(urlShortener.getCustomAlias())) {
+                throw new CustomAliasAlreadyExistsException();
+        }
+            urlShortener.setAlias(urlShortener.getCustomAlias());
+        }
+        else {
+            urlShortener.setAlias(generateRandomAlias());
+        }
+        return repository.save(urlShortener);
+    }
+
+    public UrlShortener findOrFail(String alias){
+        return repository.findByAlias(alias)
+                .orElseThrow(() -> new UrlNotFoundException(alias));
+    }
+
+    private String generateRandomAlias() {
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
 }
