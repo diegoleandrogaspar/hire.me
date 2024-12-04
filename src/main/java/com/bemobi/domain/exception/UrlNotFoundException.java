dc78c9e7fb29c342f19fc68a79c23d98f718fca6
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-12-04 20:44:26 -0300

    adicionando exceção de Url não encontrada

--- src/main/java/com/bemobi/domain/exception/UrlNotFoundException.java
+++ src/main/java/com/bemobi/domain/exception/UrlNotFoundException.java
@@ -1,2 +1,9 @@
-package com.bemobi.domain.exception;public class UrlNotFoundException {
+package com.bemobi.domain.exception;
+
+public class UrlNotFoundException extends RuntimeException{
+
+    public UrlNotFoundException(String alias) {
+        super(String.format("ERR_CODE: 002, Description: SHORTENED URL NOT FOUND for alias '%s' ", alias));
+    }
 }
+
