a3f8b056d1f57a16a2bd10d562da816c10be12d8
diegoleandrogaspar <diegolgaspar000@gmail.com>
2024-12-04 20:41:45 -0300

    adicionando Problem

--- src/main/java/com/bemobi/api/exceptionhandler/Problem.java
+++ src/main/java/com/bemobi/api/exceptionhandler/Problem.java
@@ -1,2 +1,17 @@
-package com.bemobi.api.exceptionhandler;public class Problem {
+package com.bemobi.api.exceptionhandler;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Getter
+@Builder
+public class Problem {
+
+    private String err_code;
+    private String description;
 }
